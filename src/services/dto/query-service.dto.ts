7d99cb532ecea1da203db73ffd4c0d80a1d72383
mmraisi <mahmoudmraisi2@gmail.com>
2025-07-31 01:53:06 -0400

    feat(sort): add sorting for services GET route

--- src/services/dto/query-service.dto.ts
+++ src/services/dto/query-service.dto.ts
@@ -1,10 +1,21 @@
-import { IsOptional, IsString } from 'class-validator'
+import { IsIn, IsOptional, IsString, ValidateNested } from 'class-validator'
 import { ApiPropertyOptional } from '@nestjs/swagger'
 import { PaginationDto } from '../../common/dto/pagination.dto'
+import { Type } from 'class-transformer'
 
 export class QueryServiceDto extends PaginationDto {
   @ApiPropertyOptional({ description: 'Fuzzy filter by service name' })
   @IsOptional()
   @IsString()
     service_name?: string
+
+  @ApiPropertyOptional({ description: 'Sort field', example: 'date_created' })
+  @IsOptional()
+  @IsString()
+    sort_field?: string
+
+  @ApiPropertyOptional({ description: 'Sort direction', example: 'DESC', enum: ['ASC', 'DESC'] })
+  @IsOptional()
+  @IsIn(['ASC', 'DESC'])
+    sort_direction?: 'ASC' | 'DESC'
 }
